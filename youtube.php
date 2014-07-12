1d277b8d2d9e359dc4ec7ee8daecd2facaeffd64
t-rects <info@t-rects.com>
2014-07-12 13:53:09 +0200

    initial import

---
+++ youtube.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="T-Rects : Two Little Robots And Their Pet Dinosaur">
+<meta name="keywords" content="Animation, Funny">
+<meta name="author" content="Victor Geere">
+<title>T-Rects : Two Little Robots And Their Pet Dinosaur</title>
+</head>
+<body>
+
+<iframe width="853" height="480" src="//www.youtube.com/embed/JCoLBoyBoRI?rel=0" frameborder="0" allowfullscreen></iframe>
+
+<a href="https://plus.google.com/116704429751543409287" rel="publisher">Google+</a>
+
+<!-- Place this tag where you want the share button to render. -->
+<div class="g-plus" data-action="share"></div>
+
+<!-- Place this tag after the last share tag. -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'en-GB'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+</body>
+</html>
